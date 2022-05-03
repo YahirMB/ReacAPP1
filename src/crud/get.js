c8b80340d91da4510ee8b90f681cb3532732af5a
YahirMB <alexandermanjarrez18@gmail.com>
2022-05-02 23:30:09 -0500

    cambios en get update

--- src/crud/get.js
+++ src/crud/get.js
@@ -1,9 +1,9 @@
 
 export const getGif = async (category) => {
 
-        const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&apiKey=KO1hiYGlLJM1kkCPK77kEaCaxFEKrwLH`
+        const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&apiKey=KO1hiYGlLJM1kkCPK77kEaCaxFEKrwLH`
 
-        const resp = await fetch(url);
+        const resp = fetch(url);
 
         const { data } = await resp.json();
 
